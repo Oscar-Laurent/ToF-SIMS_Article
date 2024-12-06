6508c60b37ad60a2aa76fdcdca88f8ef1e3812d0
Oscar Laurent <130485924+Oscar-Laurent@users.noreply.github.com>
2024-12-06 11:08:46 +0100

    Update data_processing.R
    Remove of the plot or the additional oxides trends

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -580,36 +580,3 @@ results <- results %>%
   select(oxides, pairwise_results) %>% 
   unnest(pairwise_results) %>%
   print(n = Inf)
-
-
-
-# =========================================================
-#  - Reviewer Comments: Plot of differents oxidation states
-# =========================================================
-
-sample_colors <- c( "Ru-Pd" = "#ffd479",
-                   "Ru-Pt-Pd" = "#509bdd", "Ru-Pt-Pd-Ir" = "#8d85e5", 
-                   "HEA Mix" = "#b373d0", "HEA Seg" = "#D452AA")
-
-
-scaled_automatic_negative_df %>% 
-  subset(primary_ion == "Bi5" &
-  sample %in% c("Ru-Pd", "Ru-Pt-Pd", "Ru-Pt-Pd-Ir", "Ru-Pt-Pd-Ir-Rh", "HEA19")) %>%
-  select(sample, Pt, matches("^PdO\\d?$")) %>% 
-  group_by(sample) %>% 
-  summarise(across(where(is.numeric), 
-                   list(mean=~mean(.x),
-                        se = ~sd(.x)/sqrt(n())))) %>% 
-  pivot_longer(!c(sample), names_sep = "_", names_to = c("oxide", "metric")) %>%
-  pivot_wider(names_from = metric, values_from = value) %>% 
-  ggplot(aes(x = sample, y = mean, color = oxide, group = oxide)) +
-  geom_point() + 
-  geom_errorbar(aes(ymin = mean - 1.96*se, ymax = mean + 1.96*se), width = 0.15) +
-  geom_line() + 
-  scale_x_discrete(labels=c("Ru-Pd" = "Ru-Pd", "RuPtPd" = "Ru-Pt-Pd",
-                               "RuPtPdIr" = "Ru-Pt-Pd-Ir", "Ru-Pt-Pd-Ir-Rh" = "HEA Mix",
-                            "HEA19" = "HEA Seg"))+
-  ylab("Scaled Intensities") + 
-  theme(legend.title = element_blank(), #change legend title font size
-        legend.text = element_text(size=14, face = "bold"),
-        axis.text.x = element_text(size = 14, angle = 45, hjust = 1, color = sample_colors, face = "bold"))
